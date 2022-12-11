b63ce276cabee7827cbc552bb12be1742d333ef2
Kinyua <alfkinyua@gmail.com>
2022-12-11 12:57:13 +0300

    Add game logic

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-const guess = Number(document.querySelector('.guess').value);
-const check = document.querySelector('.cl');
-const startGuessing = document.querySelector('.start_guessing');
+const sguess = document.querySelector('.start_guessing');
+const secret_number = Math.floor(Math.random() * 20) + 1;
+const random = (document.querySelector('.random').textContent = secret_number);
+const check = document.querySelector('.check');
+let score = 18;
+
 check.addEventListener('click', () => {
-  if (!guess) {
-    startGuessing.innerHTML = `<h1>Please enter a number<h1/>`;
-   
+  const numGuess = Number(document.querySelector('.guess').value);
+  if (!numGuess) {
+    sguess.innerHTML = `Add a number`;
+  } else if (numGuess === random) {
+    sguess.innerHTML = `correct`;
+  } else if (numGuess > random) {
+    if (score == 0) {
+      sguess.innerHTML = `Attempted too many times`;
+    } else {
+      sguess.innerHTML = `Too HIgh`;
+      score--;
+      const scores = Number(
+        (document.querySelector('.scores').textContent = score)
+      );
+    }
+  } else if (numGuess < random) {
+    if (score == 0) {
+      sguess.innerHTML = `Attempted too many times`;
+    } else {
+      sguess.innerHTML = `Too Low`;
+      score--;
+      const scores = Number(
+        (document.querySelector('.scores').textContent = score)
+      );
+    }
   }
 });
